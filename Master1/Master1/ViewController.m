57308be031c4cca41d6b0bf5a721568fd65190a2
Davide Morelli <info@davidemorelli.it>
2014-11-07 11:04:19 +0100

    example of id and SEL

--- Master1/Master1/ViewController.m
+++ Master1/Master1/ViewController.m
@@ -22,6 +22,15 @@
     Dice *dice = [[Dice alloc] init];
     int num = [dice roll];
     NSLog(@"%i", num);
+
+    // id is the type of the object instances
+    id a = dice;
+    // SEL is the type of the selectors
+    SEL r = @selector(roll);
+    // at runtime I can resolve a selector and invoke it on an instance
+    num = [a performSelector:r withObject:a];
+    NSLog(@"%i", num);
+    
 }
 
 - (void)didReceiveMemoryWarning {
