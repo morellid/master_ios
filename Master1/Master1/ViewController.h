476ef08b4218f64db1e2f2c105cf7b7de950cead
Davide Morelli <info@davidemorelli.it>
2014-11-07 10:26:36 +0100

    empty project

---
+++ Master1/Master1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Master1
+//
+//  Created by Davide Morelli on 07/11/14.
+//  Copyright (c) 2014 unipi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Master1/Master1/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Master1
+//
+//  Created by Davide Morelli on 07/11/14.
+//  Copyright (c) 2014 unipi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
